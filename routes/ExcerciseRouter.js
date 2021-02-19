bdaa5a10805db0739ce5ed740aaf83c1674620e4
unknown <wbrown152020@gmail.com>
2021-02-18 19:53:05 -0800

    Designed and developed signin form UI

--- routes/ExcerciseRouter.js
+++ routes/ExcerciseRouter.js
@@ -3,4 +3,7 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   res.render("index", { greeting: "Hello there" });
 });
+router.get("/signin", (req, res) => {
+  res.render("signin");
+});
 module.exports = router;
