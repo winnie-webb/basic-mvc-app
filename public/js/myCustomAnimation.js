f3ee3d6c4e91d28c2386766e97833857eeabd946
winnie-webb <wbrown152020@gmail.com>
2021-03-19 12:27:24 -0400

    Added Custom onscroll animation

---
+++ public/js/myCustomAnimation.js
@@ -0,0 +1,16 @@
+const allElementsInDOM = document.querySelectorAll("*");
+
+allElementsInDOM.forEach((element) => {
+  const doesElementHaveAttribute = element.getAttribute("data-animate");
+  if (doesElementHaveAttribute) {
+    const elementAnimation = element.getAttribute("data-animate");
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(elementAnimation);
+        }
+      });
+    });
+    observer.observe(element);
+  } else return;
+});
