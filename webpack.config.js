9aa8f016188d545d463e7fa464abfabd8f663a56
winnie-webb <wbrown152020@gmail.com>
2021-07-07 14:17:23 -0500

    Removed animations (for now) and modularizedejs partials

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  entry: ["./public/js/barba-init.js", "./public/js/myCustomAnimation.js"],
   output: {
     filename: "main.js",
     path: path.join(__dirname, "/public", "js"),
